66da52047638d00e93350a09bebeee9dcbc0ef9c
Peter Vyvey <peter.vyvey@gmail.com>
2016-07-08 10:42:33 +0200

    revert change

--- src/directive/angular-tree-dnd.js
+++ src/directive/angular-tree-dnd.js
@@ -959,7 +959,7 @@ function fnInitTreeDnD($timeout, $http, $compile, $parse, $window, $document, $t
                 } else {
                     _data = oData;
                 }
-            } else if (!angular.isArray($scope.treeData)) {
+            } else if (!angular.isArray($scope.treeData) || $scope.treeData.length === 0) {
                 return init_data([]);
             } else {
                 _data = $scope.treeData;
